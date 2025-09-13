984419ac4a71197fde3f6eb7b71083a135e5cd09
guptahimu90@gmail.com <guptahimu90@.com>
2025-09-13 13:53:19 +0530

    Initial commit - ready for Vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist' // Vercel ke liye build output folder
+  }
 })
